458bffaadbd62e45ebd64c3e743df4747df25175
basopro1511 <nguyenquochoang2003ct@gmail.com>
2025-07-13 01:15:00 +0700

    feat: complete header, nav, filter, first page grid course

--- edu-market/src/App.tsx
+++ edu-market/src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import { ThemeProvider } from "./components/ui/theme-providers";
 import { AppRoute } from "./routes/route";
 
 function App() {
   return (
     <>
-      <AppRoute></AppRoute>
+      {" "}
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <AppRoute></AppRoute>{" "}
+      </ThemeProvider>
     </>
   );
 }
